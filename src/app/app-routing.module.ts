d2767f259df6b9ddc9be15f9ad69d5f834a94102
nancy.kigotho@student.moringaschool.com <nancykigotho14@gmail.com>
2021-08-26 20:11:03 +0300

    add a card in the landing page.html file

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { RepositoryPageComponent } from './repository-page/repository-page.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LandingPageComponent} from './landing-page/landing-page.component';
 import { UsernamePageComponent } from './username-page/username-page.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 
 const routes: Routes = [
+  { path: '', component: LandingPageComponent },
   { path: 'search-username', component: UsernamePageComponent },
   { path: 'search-repository', component: RepositoryPageComponent },
   { path:'**', component: NotFoundComponent },
