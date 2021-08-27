97d6ffc3169137d2e7ae0b30ecf2dfd5dd98e0a9
nancy.kigotho@student.moringaschool.com <nancykigotho14@gmail.com>
2021-08-27 16:39:57 +0300

    update the user.ts

--- src/app/username.ts
+++ src/app/username.ts
@@ -1,2 +1,12 @@
 export class Username {
+    constructor(
+        public name: string,
+        public avatar_url: string,
+        public html_url: string,
+        public location: string,
+        public bio: string,
+        public twitter_username: string,
+        public public_repos: number,
+        public created_at: Date,
+      ) {}
 }
